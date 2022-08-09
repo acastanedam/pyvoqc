e0c9abbb3f3624b97a573a7538bc6337a44b7279
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2022-08-09 14:27:30 +0200

    fix_install: install python library

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo "dune build lib/libvoqc.so"
 dune build lib/libvoqc.so
 echo "" # idk why dune doesn't print a newline -KH
-echo "pip install ."
-pip install .
 echo "cp _build/default/lib/libvoqc.so pyvoqc/lib/"
 mkdir -p pyvoqc/lib
 cp _build/default/lib/libvoqc.so pyvoqc/lib/
+echo "pip install ."
+pip install .
